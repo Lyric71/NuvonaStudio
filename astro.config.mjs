bb42c93c65e80b00d5f99e0c6ed54836b2f0ea0b
Lyric71 <cyril.drouin@outlook.com>
2026-03-23 15:55:50 +0800

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nuvora.studio',
   output: 'static',
   adapter: vercel(),
   i18n: {
